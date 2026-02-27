43acad6a04a6858d9d5c8eca7b8a4e921a416dad
Yasarahasini <yasarahasini61@gmail.com>
2026-02-27 09:52:23 +0530

    create backend auth

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -1,5 +1,4 @@
-import AuthForm from "./components/AuthForm";
-
+import AuthForm from "@/app/components/AuthForm";
 export default function LoginPage() {
   return <AuthForm type="login" />;
 }
--- frontend/app/(auth)/signup/page.tsx
+++ frontend/app/(auth)/signup/page.tsx
@@ -1,4 +1,4 @@
-import AuthForm from "./components/AuthForm";
+import AuthForm from "@/app/components/AuthForm";
 
 export default function SignupPage() {
   return <AuthForm type="signup" />;
