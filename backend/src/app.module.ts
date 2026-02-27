e8dc791d10b03e5fa71b5c040ffc6f27a492df9a
Yasarahasini <yasarahasini61@gmail.com>
2026-02-27 09:15:40 +0530

    create backend auth

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { User } from './auth/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1234', 
+      database: 'plantventory',
+      entities: [User],
+      synchronize: true, 
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
