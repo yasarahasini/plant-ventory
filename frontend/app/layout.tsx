217407bfaea1e7f9007fcdf080ea498f36ac2eb3
Yasarahasini <yasarahasini61@gmail.com>
2026-02-27 10:27:56 +0530

    create backend auth

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system">
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
       </body>
