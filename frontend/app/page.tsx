4d986a215eb2b12cc90530defcc6669db48a1d94
Yasarahasini <yasarahasini61@gmail.com>
2026-02-27 12:26:44 +0530

    create footer

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import ThemeToggle from "./components/ThemeToggle";
-
 export default function HomePage() {
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -9,10 +7,8 @@ export default function HomePage() {
   };
 
   return (
-    <div className="min-h-screen  flex items-center justify-center bg-gradient-to-br from-green-50 to-emerald-100 dark:from-zinc-900 dark:to-black px-4">
-      <ThemeToggle />
-
-      <div className="bg-white dark:bg-zinc-800 shadow-xl rounded-2xl p-8 text-center">
+    <div className="min-h-screen flex items-center justify-center bg-white px-4">
+      <div className="bg-white shadow-xl rounded-2xl p-8 text-center">
         <h1 className="text-3xl font-bold text-green-600 mb-4">
           🌿 Welcome to Plantventory
         </h1>
