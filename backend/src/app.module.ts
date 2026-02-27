d36f7a37bc410e317a742e9d23a85a9743df7abd
Yasarahasini <yasarahasini61@gmail.com>
2026-02-27 09:34:41 +0530

    create backend auth

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { User } from './auth/user.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
