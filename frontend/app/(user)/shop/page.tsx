5df7fcb7421ea22c6cc2710784eb63f9fdfd86b8
Yasarahasini <yasarahasini61@gmail.com>
2026-03-15 19:13:14 +0530

    update pages

--- frontend/app/(user)/shop/page.tsx
+++ frontend/app/(user)/shop/page.tsx
@@ -159,7 +159,7 @@ function PlantCard({ plant }: { plant: Plant }) {
         </div>
 
         <button className="mt-8 w-full bg-emerald-50 text-emerald-700 py-3 rounded-xl font-bold hover:bg-emerald-900 hover:text-white transition-all duration-300">
-       Add To Cart
+       
         </button>
       </div>
     </div>
